78f8c9c0f278f93ec4277297d24c50b8baeadda4
Erik Mkrtchyan <kire45@gmail.com>
2021-08-18 14:34:30 +0300

    regexpadded

--- frontend/static/js/views/PostView.js
+++ frontend/static/js/views/PostView.js
@@ -3,12 +3,15 @@ import AbstractView from "./AbstractView.js";
 export default class extends AbstractView {
     constructor(params) {
         super(params);
+        this.postId = params.id;
         this.setTitle("Posts");
     }
 
     async getHtml() {
+        console.log(this.postId);
         return `
-            <h1>Posts or not the posts?..</h1>
-        `
+            <h1>Posts</h1>
+            <p>You are viewing posts!</p>
+        `;
     }
 }
